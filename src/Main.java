7b87e0c08c2258dcd2dac192724d47679728a295
iShantaram <1agapovav987@gmail.com>
2022-10-10 16:09:29 +0300

    Переменная dog инициализирована как тип double Сделана 1 часть дз (1-3 задачи).

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // HW1
+        // Task 1
+        System.out.println("\nTask 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("Dog: " + dog + "\nCat: " + cat + "\nPaper: " + paper);
+
+        // Task 2
+        System.out.println("\nTask 2");
+        dog += 4;
+        cat += 4;
+        paper += 4;
+
+        System.out.println("Dog: " + dog + "\nCat: " + cat + "\nPaper: " + paper);
+
+        // Task 3
+        System.out.println("\nTask 3");
+        //dog = (int) (dog - 3.5);
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+
+        System.out.println("Dog: " + dog + "\nCat: " + cat + "\nPaper: " + paper);
+
+        System.out.println("\n ~ ~ ~ ~ ~ The end ~ ~ ~ ~ ~ \n");
     }
 }
